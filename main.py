fd4e460b7b219714c1975e00a420355f319b3526
learningforevaeva <foreverlearning8376@proton.me>
2025-09-29 19:14:17 -0700

    import count_words function to stats.py

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
-def get_book_text(filepath):
+from stats import count_words
 
+def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
-    
-def count_words(text):
-    words = text.split()
-    return len(words)
 
 def main():
     text = get_book_text("books/frankenstein.txt")
-    
     word_count = count_words(text)
-
     print(f"Found {word_count} total words")
 
 if __name__ == "__main__":
