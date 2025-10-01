1917a68c0bfb1b91329dcfc3d6b9bf40ffbe6999
learningforevaeva <foreverlearning8376@proton.me>
2025-09-30 21:26:10 -0700

    Finish project

--- main.py
+++ main.py
@@ -1,15 +1,38 @@
-from stats import count_words, character_count
+from stats import (
+    count_words,
+    character_count,
+    chars_dict_to_sorted_list,
+)
+import sys
+
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
 
 def main():
-    text = get_book_text("books/frankenstein.txt")
+    book_path = sys.argv[1]
+    text = get_book_text(sys.argv[1])
     word_count = count_words(text)
     characters = character_count(text)
-    print(f"Found {word_count} total words")
-    print(characters)
+    chars_sorted_list = chars_dict_to_sorted_list(characters)
+    print_report(book_path, word_count, chars_sorted_list)
 
+def print_report(book_path, word_count, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
+    
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -9,3 +9,14 @@ def character_count(text):
         count = result.get(letter, 0)
         result[letter] = count +1
     return result
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
